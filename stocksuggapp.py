eec13b141f8636cb28f4b7878e1d5eb275c0cb85
rajenpr <pragul.rajendran@gmail.com>
2023-12-11 12:57:59 +0530

    Update stocksuggapp.py

--- stocksuggapp.py
+++ stocksuggapp.py
@@ -1,19 +1,56 @@
 import streamlit as st
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
+import pymongo
+import yfinance as yf
 
-# Streamlit interface for MongoDB connection testing
-st.title('MongoDB Connection Test')
+# MongoDB Connection String (Replace with your actual credentials)
+mongo_conn_str = st.secrets["mongodb+srv://prajendr:Mazda123@cluster0.5hmuadl.mongodb.net/?retryWrites=true&w=majority"]
 
-uri = st.text_input("Enter your MongoDB URI", value="mongodb+srv://<username>:<password>@<your-cluster-url>")
+# Initialize connection to MongoDB
+client = pymongo.MongoClient(mongo_conn_str)
+db = client.get_default_database()
+collection = db.stocksuggappcol
 
-if st.button('Test Connection'):
-    try:
-        # Create a new client and connect to the server
-        client = MongoClient(uri, server_api=ServerApi('1'))
-        # Send a ping to confirm a successful connection
-        client.admin.command('ping')
-        st.success("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        st.error(f"Error: {e}")
+def insert_trade(data):
+    collection.insert_one(data)
+
+def get_open_trades():
+    return list(collection.find({"status": "open"}))
+
+def get_current_price(ticker):
+    stock = yf.Ticker(ticker)
+    return stock.history().tail(1)['Close'].iloc[0]
+
+# Streamlit Interface
+st.title("Stock Trading App")
+
+# Input form
+with st.form("stock_input"):
+    st.subheader("Enter New Trade Details")
+    stock_name = st.text_input("Stock Name")
+    ticker_symbol = st.text_input("Ticker Symbol")
+    entry_price = st.number_input("Entry Price", min_value=0.0, format="%.2f")
+    target_price = st.number_input("Target Price", min_value=0.0, format="%.2f")
+    duration = st.selectbox("Duration of Trade", ["Short Term", "Medium Term", "Long Term"])
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        trade_data = {
+            "stock_name": stock_name,
+            "ticker_symbol": ticker_symbol,
+            "entry_price": entry_price,
+            "target_price": target_price,
+            "duration": duration,
+            "status": "open"
+        }
+        insert_trade(trade_data)
+        st.success("Trade data submitted!")
+
+# Display open trades
+if st.button("Show Open Trades"):
+    open_trades = get_open_trades()
+    for trade in open_trades:
+        current_price = get_current_price(trade['ticker_symbol'])
+        potential_gain = ((trade['target_price'] - current_price) / current_price) * 100
+        st.write(f"Stock: {trade['stock_name']} ({trade['ticker_symbol']})")
+        st.write(f"Entry Price: {trade['entry_price']}, Target Price: {trade['target_price']}, Current Price: {current_price}")
+        st.write(f"Potential Gain: {potential_gain:.2f}%")
 
