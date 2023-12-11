0f939f15c7d794cb59329678efccd71e45136502
rajenpr <pragul.rajendran@gmail.com>
2023-12-11 14:55:53 +0530

    Update stocksuggapp.py

--- stocksuggapp.py
+++ stocksuggapp.py
@@ -72,8 +72,8 @@ if st.button("Show Open Trades"):
         })
 
     # Convert list to DataFrame
-       trades_df[['Entry Price', 'Target Price', 'Current Price', 'Potential Gain (%)']] = \
-        trades_df[['Entry Price', 'Target Price', 'Current Price', 'Potential Gain (%)']].round(2)
+       trades_df[['Entry Price', 'Target Price', 'Current Price', 'Potential Gain (%)']] = trades_df[['Entry Price', 'Target Price', 'Current Price', 'Potential Gain (%)']].round(2)
+        
 
     # Display the DataFrame as a table
     st.table(trades_df)
