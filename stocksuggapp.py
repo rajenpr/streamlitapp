58595055599a28b30c98943a1f71feac93bd9514
rajenpr <pragul.rajendran@gmail.com>
2023-12-11 14:51:53 +0530

    Update stocksuggapp.py

--- stocksuggapp.py
+++ stocksuggapp.py
@@ -67,6 +67,7 @@ if st.button("Show Open Trades"):
             "Entry Price": trade['entry_price'],
             "Target Price": trade['target_price'],
             "Current Price": current_price,
+            "Duration":trade['duration']
             "Potential Gain (%)": f"{potential_gain:.2f}"
         })
 
