f45fec47b42eeae76f69499444f0e26bee62a9d8
rajenpr <pragul.rajendran@gmail.com>
2023-12-11 17:10:59 +0530

    Update stocksuggapp.py

--- stocksuggapp.py
+++ stocksuggapp.py
@@ -18,10 +18,10 @@ def get_trades_by_status(status):
     return list(collection.find({"status": status}))
 
 def update_trade_status(trade_id, new_status, last_known_price=None):
-    if last_known_price:
-        collection.update_one({"_id": trade_id}, {"$set": {"status": new_status, "last_known_price": last_known_price}})
-    else:
-        collection.update_one({"_id": trade_id}, {"$set": {"status": new_status}})
+    update_data = {"status": new_status}
+    if last_known_price is not None:
+        update_data["last_known_price"] = last_known_price
+    collection.update_one({"_id": trade_id}, {"$set": update_data})
 
 def get_current_price(ticker):
     ticker_final = ticker + '.NS'
@@ -33,12 +33,13 @@ def get_current_price(ticker):
         return None
 
 def display_trades(trades, title):
-    table_data = []
+    active_trades_data = []
+    untriggered_trades_data = []
 
     for trade in trades:
-        if trade['status'] == 'open':
-            current_price = get_current_price(trade['ticker_symbol'])
-            if current_price is not None and current_price >= trade['target_price']:
+        current_price = get_current_price(trade['ticker_symbol']) if trade['status'] == 'open' else trade.get('last_known_price', "N/A")
+        if trade['status'] == 'open' and current_price is not None:
+            if current_price >= trade['target_price']:
                 update_trade_status(trade['_id'], "closed", last_known_price=current_price)
                 trade['status'] = "closed"
                 potential_gain_formatted = "Target Achieved"
@@ -46,10 +47,9 @@ def display_trades(trades, title):
                 potential_gain = ((trade['target_price'] - current_price) / current_price) * 100
                 potential_gain_formatted = "{:.2f}".format(potential_gain)
         else:
-            current_price = trade.get('last_known_price', "N/A")
             potential_gain_formatted = "Trade Closed"
 
-        table_data.append({
+        trade_info = {
             "Stock": f"{trade['stock_name']} ({trade['ticker_symbol']})",
             "Entry Price": "{:.2f}".format(trade['entry_price']),
             "Target Price": "{:.2f}".format(trade['target_price']),
@@ -57,11 +57,21 @@ def display_trades(trades, title):
             "Duration": trade['duration'],
             "Potential Gain (%)": potential_gain_formatted,
             "Status": trade['status']
-        })
+        }
 
-    trades_df = pd.DataFrame(table_data)
-    st.subheader(title)
-    st.table(trades_df)
+        if trade['status'] == 'open':
+            if current_price >= trade['entry_price']:
+                active_trades_data.append(trade_info)
+            else:
+                untriggered_trades_data.append(trade_info)
+
+    if active_trades_data:
+        st.subheader(title + " - Active Trades")
+        st.table(pd.DataFrame(active_trades_data))
+
+    if untriggered_trades_data:
+        st.subheader(title + " - Untriggered Trades")
+        st.table(pd.DataFrame(untriggered_trades_data))
 
 # Streamlit Interface
 st.title("Stock Trading App")
@@ -87,7 +97,7 @@ with st.form("stock_input"):
         insert_trade(trade_data)
         st.success("Trade data submitted!")
 
-# Display sections for Open and Closed trades
+# Display sections for Active, Untriggered, and Closed trades
 if st.button("Refresh Trades"):
     open_trades = get_trades_by_status("open")
     closed_trades = get_trades_by_status("closed")
