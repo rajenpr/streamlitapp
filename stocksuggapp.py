3dedfc1209aa19634293f6f584987d0f2616cbe8
rajenpr <pragul.rajendran@gmail.com>
2023-12-11 16:19:30 +0530

    Update stocksuggapp.py

--- stocksuggapp.py
+++ stocksuggapp.py
@@ -6,7 +6,6 @@ import pandas as pd
 # MongoDB Connection String (Replace with your actual credentials)
 mongo_conn_str = "mongodb+srv://prajendr:Mazda123@cluster0.5hmuadl.mongodb.net/stocksuggappdb?retryWrites=true&w=majority"
 
-
 # Initialize connection to MongoDB
 client = pymongo.MongoClient(mongo_conn_str)
 db = client["stocksuggappdb"]
@@ -15,17 +14,54 @@ collection = db.stocksuggappcol
 def insert_trade(data):
     collection.insert_one(data)
 
-def get_open_trades():
-    return list(collection.find({"status": "open"}))
+def get_trades_by_status(status):
+    return list(collection.find({"status": status}))
+
+def update_trade_status(trade_id, new_status, last_known_price=None):
+    if last_known_price:
+        collection.update_one({"_id": trade_id}, {"$set": {"status": new_status, "last_known_price": last_known_price}})
+    else:
+        collection.update_one({"_id": trade_id}, {"$set": {"status": new_status}})
 
 def get_current_price(ticker):
-    ticker_final=ticker+'.NS'
+    ticker_final = ticker + '.NS'
     stock = yf.Ticker(ticker_final)
     history = stock.history().tail(1)
     if not history.empty:
         return history['Close'].iloc[0]
     else:
         return None
+
+def display_trades(trades, title):
+    table_data = []
+
+    for trade in trades:
+        if trade['status'] == 'open':
+            current_price = get_current_price(trade['ticker_symbol'])
+            if current_price is not None and current_price >= trade['target_price']:
+                update_trade_status(trade['_id'], "closed", last_known_price=current_price)
+                trade['status'] = "closed"
+                potential_gain_formatted = "Target Achieved"
+            else:
+                potential_gain = ((trade['target_price'] - current_price) / current_price) * 100
+                potential_gain_formatted = "{:.2f}".format(potential_gain)
+        else:
+            current_price = trade.get('last_known_price', "N/A")
+            potential_gain_formatted = "Trade Closed"
+
+        table_data.append({
+            "Stock": f"{trade['stock_name']} ({trade['ticker_symbol']})",
+            "Entry Price": "{:.2f}".format(trade['entry_price']),
+            "Target Price": "{:.2f}".format(trade['target_price']),
+            "Current Price": "{:.2f}".format(current_price) if current_price != "N/A" else current_price,
+            "Duration": trade['duration'],
+            "Potential Gain (%)": potential_gain_formatted,
+            "Status": trade['status']
+        })
+
+    trades_df = pd.DataFrame(table_data)
+    st.subheader(title)
+    st.table(trades_df)
 
 # Streamlit Interface
 st.title("Stock Trading App")
@@ -51,33 +87,10 @@ with st.form("stock_input"):
         insert_trade(trade_data)
         st.success("Trade data submitted!")
 
-# Display open trades
-if st.button("Show Open Trades"):
-    open_trades = get_open_trades()
+# Display sections for Open and Closed trades
+if st.button("Refresh Trades"):
+    open_trades = get_trades_by_status("open")
+    closed_trades = get_trades_by_status("closed")
 
-    # Create a list to store our table data
-    table_data = []
-
-    for trade in open_trades:
-        current_price = get_current_price(trade['ticker_symbol'])
-        if current_price is not None:
-            potential_gain = ((trade['target_price'] - current_price) / current_price) * 100
-            potential_gain_formatted = "{:.2f}".format(potential_gain)
-        else:
-            potential_gain_formatted = "N/A"  # or some default value
-
-        # Append a dictionary for each trade to our list
-        table_data.append({
-            "Stock": f"{trade['stock_name']} ({trade['ticker_symbol']})",
-            "Entry Price": "{:.2f}".format(trade['entry_price']),
-            "Target Price": "{:.2f}".format(trade['target_price']),
-            "Current Price": "{:.2f}".format(current_price) if current_price is not None else "N/A",
-            "Duration":trade['duration'],
-            "Potential Gain (%)": potential_gain_formatted
-        })
-
-    # Convert list to DataFrame
-    trades_df = pd.DataFrame(table_data)
-
-    # Display the DataFrame as a table
-    st.table(trades_df)
+    display_trades(open_trades, "Open Trades")
+    display_trades(closed_trades, "Closed Trades")
